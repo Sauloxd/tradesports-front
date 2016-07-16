45afec7796ca0b8356e32c9d09f1238dacef62f1
Sauloxd <saulotoshi@gmail.com>
2016-07-16 18:02:31 -0300

    Etracted banner, filter, flexslider

--- src/app/pages/home/home.routes.js
+++ src/app/pages/home/home.routes.js
@@ -1,4 +1,7 @@
 import '!ng-cache!../../components/navbar/navbar.html';
+// import '!ng-cache!../../components/flexslider/flexslider.html';
+import '!ng-cache!../../components/banner/two-banner.html';
+import '!ng-cache!../../components/filter-menu/filter-menu.html';
 import '!ng-cache!./home.html';
 
 export default function routes($stateProvider) {
