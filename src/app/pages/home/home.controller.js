c743bd4c62daeadb8c0028b0d19a48baa8de3cf8
manuelpuyol <manuelpuyol@gmail.com>
2016-08-08 14:48:03 -0300

    Added pages function

--- src/app/pages/home/home.controller.js
+++ src/app/pages/home/home.controller.js
@@ -1,17 +1,33 @@
-var homeController = function ($rootScope, crudService) {
+var smoothScroll = require('smoothscroll');
+var homeController = function ($rootScope, crudService, $window) {
   var vm = this;
 
   crudService.get('produto')
-    .then(function(response){
-      vm.products = response.data.slice(0, 12);
-    }, function(err) {
-      console.log('error', err);
-    });
+	.then(function(response){
+		vm.page = 1
+		vm.allProducts = response.data
+	  vm.products = vm.allProducts.slice(0, 12);
+	}, function(err) {
+	  console.log('error', err);
+	});
 
-    $rootScope.$on('rootScope:newProducts', function (event, data) {
-    	console.log(data);
-    	vm.products = data.slice(0,12)
-  	});
+	$rootScope.$on('rootScope:newProducts', function (event, data) {
+		vm.page = 1
+		vm.allProducts = data
+		vm.products = vm.allProducts.slice(0,12)
+	});
+
+	vm.nextPage = function() {
+		vm.products = vm.allProducts.slice(vm.page * 12, (vm.page + 1) * 12)
+		vm.page++
+		smoothScroll(200, 800)
+	}
+
+	vm.lastPage = function() {
+		vm.products = vm.allProducts.slice((vm.page - 1) * 12, vm.page * 12)
+		vm.page--
+		smoothScroll(200, 800)
+	}
 }
 
 export default homeController;
